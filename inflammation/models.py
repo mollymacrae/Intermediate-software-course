6b5aea0b46023e8c440c6e16fa0302838b74c685
Molly MacRae <molly.macrae@stfc.ac.uk>
2022-11-08 10:28:01 +0000

    changes to models.py

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,88 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    maxes = np.max(data, axis=0)
+    return data / maxes[:, np.newaxis]
+
+
+def standard_deviation(data):
+    """Calculate the standard deviation of a 2D inflammation data array."""
+    return np.std(data, axis=0)
+
+
+# file: inflammation/models.py
+
+class Observation:
+    def __init__(self, day, value):
+        self.day=day
+        self.value=value
+
+    def __str__(self):
+        return str(self.value)
+
+class Person:
+    #anything that all people have in common defined here
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name: str):
+        
+        super().__init__(name) #super accesses the parent class that you've inherited from (Person)
+        self.observations = []
+
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+class Doctor(Person):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.patients[]
+
+    @property
+    def add_patient(self):
+        return [p.name for p in self.patients]
+
+    def add_patient(self, new_patient):
+        #check pateint is not already assigned to this doctor
+        for patient in self.patients:
+            if patient.name == new_patient.name:
+                return
+        self.patients.append(new_patient)
+
+
+
+alice = Patient('Alice')
+
+alice.add_observation(3)
+alice.add_observation(4)
+
+obs = alice.last_observation
+print('last observation was: ', obs)
+
+print(alice.observations)
+
